9bf9e78455c5218af2af435e4b210d8812772e67
zhucong <tniub.cc@gmail.com>
2018-09-28 08:55:15 +0000

    商品列表

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -24,5 +25,13 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    public function getImageUrlAttribute()
+    {
+        if (Str::startsWith($this->attributes['image'],['http://','https://'])) {
+                  return $this->attributes['image'];
+        }
+        return \Storage::disk('public')->url($this->attributes['image']);
     }
 }
